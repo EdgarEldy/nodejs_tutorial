f986b8895fb69aebffa865a89ea1a6d724110cd4
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-23 23:29:21 +0200

    Added a function for inserting a new product category

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Category = mongoose.model('Category');
 
 router.get('/categories', function (req, res) {
-   res.render('categories/index');
+    res.render('categories/index');
 });
 
 router.get('/categories/add', function (req, res) {
-   res.render('categories/add');
+    res.render('categories/add');
 });
+
+router.post('/categories', function (req, res) {
+    add(req, res);
+});
+
+//Insert a new product category
+function add(req, res) {
+    var category = new Category();
+    category.cat_name = req.body.cat_name;
+    category.save(function (err, doc) {
+        if (!err) {
+            res.redirect('categories');
+        } else {
+            console.log('Error during record insertion :' + err);
+        }
+    });
+}
 
 module.exports = router;
