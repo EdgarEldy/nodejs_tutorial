0cf7fa2e92f720f7707adc7eb3101185f7d83518
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-24 23:59:31 +0200

    Added post method fo inserting a new product category to mongodb

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use('/users', usersRouter);
 // Categories
 app.get('/categories',categoriesRouter);
 app.get('/categories/add', categoriesRouter);
+app.post('categories', categoriesRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
